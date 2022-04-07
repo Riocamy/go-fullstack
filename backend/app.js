619c403b61a7c0d50fbb53c3079c3112eaa5e762
Chlodion Gossart <chlodiongossart@gmail.com>
2022-04-07 21:18:55 +0200

    added get road

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,49 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-  res.json({message: 'Votre requête a bien été reçue.'})
-})
+/**** Intégration des Middlewares ****/
+
+//Accès au core de la requête
+app.use(express.json());
+
+//Ajout des Middlewares d'autorisations
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+//Création de la route POST
+app.post('/api/stuff', (req, res, next) => {
+  console.log(req.body);
+  res.status(201).json({
+    message: 'Objet créé !'
+  });
+});
+
+//Création de la route GET
+app.get('/api/stuff', (req, res, next) => {
+  console.log('Requête envoyée avec succès !');
+  const stuff = [
+    {
+      _id: 'oeihfzeoi',
+      title: 'Mon premier objet',
+      description: 'Les infos de mon premier objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 4900,
+      userId: 'qsomihvqios',
+    },
+    {
+      _id: 'oeihfzeomoihi',
+      title: 'Mon deuxième objet',
+      description: 'Les infos de mon deuxième objet',
+      imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
+      price: 2900,
+      userId: 'qsomihvqios',
+    },
+  ];
+  res.status(200).json(stuff);
+});
 
 module.exports = app;
