57b6d74505b37004721e84ecf4888e1f6405eb88
Chlodion Gossart <chlodiongossart@gmail.com>
2022-04-08 19:05:22 +0200

    added put road

--- backend/app.js
+++ backend/app.js
@@ -52,4 +52,11 @@ app.get('/api/stuff', (req, res, next) => {
     .catch(error => res.status(400).json({ error }));
 });
 
+//Création de la route PUT
+app.put('/api/stuff/:id', (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet modifié !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 module.exports = app;
