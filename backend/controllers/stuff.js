344620ff8bcea61963bd48543e1998366e8fa563
Chlodion Gossart <chlodiongossart@gmail.com>
2022-04-09 16:44:39 +0200

    fixed delete route

--- backend/controllers/stuff.js
+++ backend/controllers/stuff.js
@@ -69,19 +69,33 @@ exports.modifyThing = (req, res, next) => {
 
 //Controller de la route DELETE
 exports.deleteThing = (req, res, next) => {
-  Thing.deleteOne({_id: req.params.id}).then(
-    () => {
-      res.status(200).json({
-        message: 'Deleted!'
-      });
+  Thing.findOne({ _id: req.params.id }).then(
+    (thing) => {
+      if (!thing) { //S'il n'y a pas d'objet à supprimer
+        res.status(404).json({
+          error: new Error('No such Thing!')
+        });
+      }
+      if (thing.userId !== req.auth.userId) { //Si c'est un autre utilisateur qui cherche à supprimer l'objet
+        res.status(400).json({
+          error: new Error('Unauthorized request!')
+        });
+      }
+      Thing.deleteOne({ _id: req.params.id }).then( //Si ces conditions sont OK, on applique le controller
+        () => {
+          res.status(200).json({
+            message: 'Deleted!'
+          });
+        }
+      ).catch(
+        (error) => {
+          res.status(400).json({
+            error: error
+          });
+        }
+      );
     }
-  ).catch(
-    (error) => {
-      res.status(400).json({
-        error: error
-      });
-    }
-  );
+  )
 };
 
 //Controller de la route GET (récupération de tous les objets)
--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -8,9 +8,12 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization.split(' ')[1];
     //Vérifier si la clé d'authentification du token est correcte
     const decodedToken = jwt.verify(token, 'RANDOM_TOKEN_SECRET');
-    //Récupération de l'ID de l'utilisateur
+    //Récupération de l'userId du token
     const userId = decodedToken.userId;
-    //Vérifier si l'utilisateur est le même que celui qui aenvoyé la requête
+    //Pour déclarer qu'il s'agit de l'utilisateur qui en envoyé la requête
+    req.auth = { userId };
+    /* OLD : req.userId = userId; */
+    //Vérifier si l'utilisateur est le même que celui qui envoyé la requête
     if (req.body.userId && req.body.userId !== userId) {
       throw 'Invalid user ID';
     } else {
